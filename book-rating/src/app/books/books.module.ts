bdf1805e9e2dd4062353a5c3bad1c5225fc4bf27
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-02-27 11:36:05 +0100

    form validation

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
 import { RatingDirective } from './shared/rating.directive';
+import { CreateBookComponent } from './create-book/create-book.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     BookComponent,
-    RatingDirective
+    RatingDirective,
+    CreateBookComponent
   ],
   imports: [
     CommonModule,
-    BooksRoutingModule
+    BooksRoutingModule,
+    ReactiveFormsModule
   ],
   exports: [
     DashboardComponent
---
+++ book-rating/src/app/books/create-book/create-book.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'br-create-book',
+  templateUrl: './create-book.component.html',
+  styleUrls: ['./create-book.component.scss']
+})
+export class CreateBookComponent {
+
+  bookForm = new FormGroup({
+    isbn: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+    ]),
+    title: new FormControl('', Validators.required),
+    description: new FormControl('')
+  });
+
+  isInvalid(name: string) {
+    const control = this.bookForm.get(name);
+    return control.invalid && control.dirty;
+  }
+}
