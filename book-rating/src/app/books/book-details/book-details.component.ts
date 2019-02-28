7b1614651705735dba712d232a990c76c1cfcbe2
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-02-28 11:15:38 +0100

    never do this!

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map, filter, tap, reduce, scan } from 'rxjs/operators';
 import { of, from, Observable } from 'rxjs';
+import { BookStoreService } from '../shared/book-store.service';
+import { Book } from '../shared/book';
 
 
 @Component({
@@ -11,48 +13,18 @@ import { of, from, Observable } from 'rxjs';
 })
 export class BookDetailsComponent implements OnInit {
 
-  isbn: string;
+  book: Book;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) { }
 
   ngOnInit() {
     this.route.paramMap
       .pipe(
-        map(params => params.get('isbn'))
+        map(params => params.get('isbn')),
+        map(isbn => this.bs.getSingle(isbn))
       )
-      .subscribe(isbn => this.isbn = isbn);
-
-      ////////////
-      // import { of, from } from 'rxjs';
-
-      const observer = {
-        next: e => console.log(e),
-        error: e => console.error(e),
-        complete: () => console.log('Complete')
-      };
-
-      const myObservable$ = new Observable<number>(subscriber => {
-        subscriber.next(1);
-        subscriber.next(2);
-        subscriber.next(3);
-
-        setTimeout(() => subscriber.next(4), 1000);
-        setTimeout(() => subscriber.next(5), 2000);
-
-        setTimeout(() => subscriber.complete(), 2000);
-      });
-
-      // Subscription: Vertrag zw. Observer und Observable
-      const subscription = myObservable$
-        .pipe(
-          map(x => x * 10),
-          filter(x => x > 20),
-          scan((acc, item) => acc + item, 0)
-        )
-        .subscribe(observer);
-
-      setTimeout(() => subscription.unsubscribe(), 2000);
-
+      .subscribe(x => x
+        .subscribe(book => this.book = book));
 
   }
 
