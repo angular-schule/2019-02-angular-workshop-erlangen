5cf4edbb3502c9b9071491afb75bf2889beea9c6
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-02-28 08:59:24 +0100

    showing thumbnails

--- book-rating/src/app/books/shared/book.ts
+++ book-rating/src/app/books/shared/book.ts
@@ -1,7 +1,13 @@
+export interface Thumbnail {
+  url: string;
+  title: string;
+}
+
 export interface Book {
   isbn: string;
   title: string;
   description: string;
   rating: number;
   price?: number;
+  thumbnails: Thumbnail[];
 }
