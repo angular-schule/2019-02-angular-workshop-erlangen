788f1480e9edbdb1b3c813683ea64dd543800d5d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-03-01 09:48:30 +0100

    proper errror handling

--- book-rating/src/app/books/effects/book.effects.ts
+++ book-rating/src/app/books/effects/book.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { catchError, map, concatMap } from 'rxjs/operators';
+import { catchError, map, concatMap, switchMap } from 'rxjs/operators';
 import { EMPTY, of } from 'rxjs';
 import { LoadBooksFailure, LoadBooksSuccess, BookActionTypes, BookActions } from '../actions/book.actions';
 import { BookStoreService } from '../shared/book-store.service';
@@ -10,11 +10,23 @@ import { BookStoreService } from '../shared/book-store.service';
 @Injectable()
 export class BookEffects {
 
+  /*
   @Effect()
   loadBooks$ = this.actions$.pipe(
     ofType(BookActionTypes.LoadBooks),
-    concatMap(() => this.bs.getAll()),
-    map(books => new LoadBooksSuccess({ books }))
+    concatMap(() => EMPTY.pipe(
+      map(data => new LoadBooksSuccess({ data })),
+      catchError(error => of(new LoadBooksFailure({ error }))))
+    )
+  */
+
+  @Effect()
+  loadBooks$ = this.actions$.pipe(
+    ofType(BookActionTypes.LoadBooks),
+    switchMap(() => this.bs.getAll().pipe(
+      map(books => new LoadBooksSuccess({ books })),
+      catchError(error => of(new LoadBooksFailure({ error })))
+    ))
   );
 
 
